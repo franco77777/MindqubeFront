a46ede2399acf832ad70155954dae46ad147e030
Franco Vedia <francoaresis@gmail.com>
2023-11-12 22:11:54 -0300

    testing vercel

--- src/pages/PageTest.tsx
+++ src/pages/PageTest.tsx
@@ -2,18 +2,25 @@ import { Link, useNavigate } from "react-router-dom";
 import {
   GoogleAuthProvider, //el proveedor de autenticacion
   signInWithPopup, //redirige a una ventana para autenticar
-  signOut,
-  onAuthStateChanged, //verifica el estado de nuestra autenticacion
 } from "firebase/auth";
 import { auth } from "../utils/firebaseConfig";
-import { useEffect, useState } from "react";
-import { logOut, sigIn } from "../utils/firebaseConfig";
-import Sidebar from "../components/sidebar/SideBar";
+import { useEffect, useState } from "preact/hooks";
+import { useSignal } from "@preact/signals";
+//import SockJS from "sockjs-client";
+//import { over } from "stompjs";
+// import * as Stomp from 'stompjs';
+//const Stomp = require("stompjs");
+
+import SockJS from "sockjs-client";
+
+import Stomp, { over } from "stompjs";
+//const SockJS = require("sockjs-client")
 
 const PageTest = () => {
   const [user, setUser] = useState({});
   const navigate = useNavigate();
   console.log("PageTest Render");
+
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
@@ -35,10 +42,27 @@ const PageTest = () => {
   //   }
   // }, [user]);
 
+  // const socketMessate = useSignal("");
+
+  // let socket = new WebSocket("ws://localhost:8080/chat");
+  // socket.onmessage = (event) => {
+  //   console.log(event.data);
+  //   socketMessate.value = "probando si realmente funciona";
+  // };
+
+  // const sendMessage = (e: any) => {
+  //   socket.send("enviar");
+  // };
+
   return (
     <>
       <div className="">
         <h2>PAGINA 1</h2>
+        <div>
+          <span className="">soy socket value : {socketMessages.value}</span>
+          <input type="text" name="" id="" />
+          <button onClick={connect}></button>
+        </div>
         <Link to={"/test2"}>
           <button className="bg-red-500">Page 222222</button>
         </Link>
